b817e341d0d9976fe6dd91095c4e7454e6005e82
iohan <iohantrezzecs@gmail.com>
2023-01-26 19:58:11 -0300

    Category variable working

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+let category = document.querySelector("#category").value;
+console.log(category)
+
+
 const loadData = async () => {
-    const res = await fetch("https://www.dnd5eapi.co/api/classes/paladin");
+    let category = document.querySelector("#category").value;
+    const res = await fetch(`https://www.dnd5eapi.co/api/${category}/paladin`);
     const data = await res.json();
     console.log(data)
 };
