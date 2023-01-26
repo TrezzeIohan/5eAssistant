f7929935e797c74f7fae2a0d5217ed2ce014066b
iohan <iohantrezzecs@gmail.com>
2023-01-26 19:46:28 -0300

    Search Button is working

--- app.js
+++ app.js
@@ -2,5 +2,8 @@ const loadData = async () => {
     const res = await fetch("https://www.dnd5eapi.co/api/classes/paladin");
     const data = await res.json();
     console.log(data)
-}
+};
 
+const searchButton = document.querySelector("#searchButton");
+
+searchButton.addEventListener('click', loadData)
