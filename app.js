44e8872ac78fc5a6c275f3179135231098e44193
iohan <iohantrezzecs@gmail.com>
2023-01-26 21:13:26 -0300

    Added string manipulation to the search term. Lowercased and changed the blank spaces with -

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-let category = document.querySelector("#category").value;
-console.log(category)
+
+
 
 
 const loadData = async () => {
+
+    let searchTerm = ((document.querySelector("#search-term").value).toLowerCase()).replaceAll(' ', '-');
     let category = document.querySelector("#category").value;
-    const res = await fetch(`https://www.dnd5eapi.co/api/${category}/paladin`);
+    const res = await fetch(`https://www.dnd5eapi.co/api/${category}/${searchTerm}`);
     const data = await res.json();
     console.log(data)
+    
 };
 
 const searchButton = document.querySelector("#searchButton");
