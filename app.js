0d1030057d8a1236069bf281a4a3d507d7b8ba4d
iohan <iohantrezzecs@gmail.com>
2023-01-27 11:41:33 -0300

    Don't know the change

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const loadData = async () => {
     let category = document.querySelector("#category").value;
     const res = await fetch(`https://www.dnd5eapi.co/api/${category}/${searchTerm}`);
     const data = await res.json();
-    console.log(data)
-    
+    console.log(data);
+    return data;
 };
 
 const searchButton = document.querySelector("#searchButton");
