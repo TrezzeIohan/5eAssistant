026c40ebb15c261659da156531543207bdcb2490
iohan <iohan.trezze@gmail.com>
2023-02-01 08:47:31 -0300

    Started making race category

--- app.js
+++ app.js
@@ -85,6 +85,78 @@ const displayData = function(category, data){
         tdDesc.innerText = description;
         tRow.appendChild(tdDesc);
     }
+    else if (category === "races"){
+        // Target the data container div
+        const dataContainer = document.querySelector("#dataContainer");
+
+        // Create and append the table to display the new data
+        const table = document.createElement("table");
+        table.classList.add("table");
+        table.classList.add("table-dark")
+        dataContainer.append(table);
+
+        // Create the table head
+        const tableHead = document.createElement("thead");
+        //tableHead.classList.add("thead-light");
+        table.appendChild(tableHead);
+
+        // Create a table row for the th's
+        const tableRow = document.createElement("tr");
+        tableHead.appendChild(tableRow);
+
+        // Create adn append the table h's
+        const thIndex = document.createElement("th");
+        thIndex.setAttribute("scope", "col");
+        thIndex.textContent = "#";
+        tableRow.appendChild(thIndex);
+
+        const thName = document.createElement("th");
+        thName.setAttribute("scope", "col");
+        thName.textContent = "Name";
+        tableRow.appendChild(thName);
+
+        const thRarity = document.createElement("th");
+        thRarity.setAttribute("scope", "col");
+        thRarity.textContent = "Rarity";
+        tableRow.appendChild(thRarity);
+
+        const thDescription = document.createElement("th");
+        thDescription.setAttribute("scope", "col");
+        thDescription.textContent = "Item Description";
+        tableRow.appendChild(thDescription);
+
+        
+
+
+        // Create the table body
+        const tableBody = document.createElement("tbody");
+        table.appendChild(tableBody);
+
+        // Create tr
+        const tRow = document.createElement("tr");
+        tableBody.appendChild(tRow);
+
+        // Create the th and append it the TRow
+        const thData = document.createElement("th");
+        thData.setAttribute("scope", "row");
+        tRow.appendChild(thData);
+
+        //Create the table cols and append to the TRow
+        const tdName = document.createElement("td");
+        const name = data.name;
+        tdName.innerText = name;
+        tRow.appendChild(tdName);
+
+        const tdRarity= document.createElement("td");
+        const rarity = data.desc[0];
+        tdRarity.innerText = rarity;
+        tRow.appendChild(tdRarity);
+
+        const tdDesc= document.createElement("td");
+        const description = data.desc[1];
+        tdDesc.innerText = description;
+        tRow.appendChild(tdDesc);
+    }
 
 
 }
@@ -98,7 +170,7 @@ const loadAndDisplayData = function() {
     //Load Data
     data = loadData(category, searchTerm);
     data.then(data =>{
-        console.log(data);
+        //console.log(data);
         displayData(category, data)
     });
     
